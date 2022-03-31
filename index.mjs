77352d0fc3bffe681badafd955c6879737ccbc2b
Yehia Tarek <yehia@startfi.io>
2022-03-31 16:52:57 +0200

    add interact transactions

--- index.mjs
+++ index.mjs
@@ -24,12 +24,22 @@ const [houseA, houseB, houseC] = await Promise.all([
     supply: 1,
   }),
 ]);
+console.log("accOwner", accOwner.getAddress());
+console.log("house A is", houseA);
 
-const rentA = stdlib.parseCurrency(2);
-const rentB = stdlib.parseCurrency(3);
-const rentC = stdlib.parseCurrency(4);
+// A fast solution to manage ownership in Reach
+// To be modified by adding a token protocol and modify it
+const nftIdByOwner = {};
+// fill object with NFTs id and the owner
+nftIdByOwner[houseA.id.toString()] = accOwner.getAddress();
+nftIdByOwner[houseB.id.toString()] = accOwner.getAddress();
+nftIdByOwner[houseC.id.toString()] = accOwner.getAddress();
 
-const unixTime = stdlib.unixTime();
+const rentAPrice = stdlib.parseCurrency(2);
+const rentBPrice = stdlib.parseCurrency(3);
+const rentCPrice = stdlib.parseCurrency(4);
+
+const unixTime = Date.now() * 1000; // in millisecond
 const houseAPaymentsDates = [
   unixTime + 1000,
   unixTime + 2000,
@@ -56,17 +66,47 @@ const ctcOwner = accOwner.contract(backend);
 await ctcOwner.participants.Owner({
   rentNFT: (newNft) => {
     const { nftId, owner, renter, rentAmount, paymentDates } = newNft;
+    if (nftIdByOwner[nftId] !== owner) {
+      console.error(`Owner ${owner} is not the owner of NFT ${nftId}`);
+    }
+
     rentedNfts[nftId] = {
       owner,
       renter,
       rentAmount,
       paymentDates,
     };
+    // transfer ownership
+    nftIdByOwner[nftId] = renter;
+
+    for (let i = 0; i < paymentDates.length; paymentDates.length) {
+      rentersPayment[paymentDates[i]] = {
+        nftId,
+        renter,
+        rentAmount,
+        hasPayed: false,
+      };
+    }
     return true;
+  },
+  claimNft: (nftId) => {
+    // msg.sender in Reach?
+    const todayInMillisecond = Date.now().getTime() * 1000;
+    for (let i = 0; i < rentedNfts[nftId].paymentDates.length; i++) {
+      if (todayInMillisecond > rentedNfts[nftId].paymentDates[i]) {
+        if (rentersPayments[rentedNfts[nftId].paymentDates[i]].hasPayed)
+          // Pay renting Amount:  rentersPayments[rentedNfts[nftId].paymentDates[i]].rentAmount
+          // return true to pay the rent on the .rsh
+          return true;
+      } else {
+        nftIdByOwner[nftId] = rentedNfts[nftId].owner;
+        return false;
+      }
+    }
   },
 });
 
-const ctcRenter = accRenter.contract(backend);
+const ctcRenter = accRenter.attach(backend, ctcOwner.getInfo());
 await ctcRenter.participants.Renter({
   payRent: (rent) => {
     const { paymentDate, nftId, renter, rentAmount } = rent;
